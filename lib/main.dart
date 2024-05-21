989b6052b58115bf39d0538c178d6964b8815960
Croppedeyebrow <lee940706@gmail.com>
2024-05-21 21:37:57 +0900

    보안

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:flutter_chapapp_firebase/auth/login_or_register.dart';
 import 'package:flutter_chapapp_firebase/firebase_options.dart';
 
 import 'package:flutter_chapapp_firebase/themes/light_mode.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
