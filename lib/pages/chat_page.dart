35122cdc05dde411b08269db75848ced84aa40f8
Croppedeyebrow <lee940706@gmail.com>
2024-05-23 20:49:24 +0900

    0523-2

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,33 @@
+import "package:cloud_firestore/cloud_firestore.dart";
 import "package:flutter/material.dart";
+import "package:flutter_chapapp_firebase/components/my_textfield.dart";
+import "package:flutter_chapapp_firebase/service/auth/auth_service.dart";
+import "package:flutter_chapapp_firebase/service/chat/chat_services.dart";
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
-  const ChatPage({super.key, required this.receiverEmail});
+  final String receiverID;
+
+  ChatPage({super.key, required this.receiverEmail, required this.receiverID});
+
+  //대화 컨트롤러
+  final TextEditingController _messageConroller = TextEditingController();
+
+  // 채팅 & 회원
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  // 메세지 보내기
+  void sendMessage() async {
+    //입력창 안에 내용이 있다면?
+    if (_messageConroller.text.isNotEmpty) {
+      //메세지를 보내기
+      await _chatService.sendMessage(receiverID, _messageConroller.text);
+
+      //채팅방 초기화.
+      _messageConroller.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +35,61 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(receiverEmail),
       ),
+      body: Column(
+        children: [
+          //메시지 출력하기.
+          Expanded(
+            child: _buildMessageList(),
+          )
+
+          // 텍스트 입력
+        ],
+      ),
+    );
+  }
+
+  //메세지 리스트 만들기
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessages(receiverID, senderID),
+      builder: (context, snapshot) {
+        //erros
+        if (snapshot.hasError) {
+          return const Text("에러");
+        }
+
+        //laoding
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("로딩중...");
+        }
+        //return list view
+        return ListView(
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  //build messageitem
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  //메세지 입력
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(
+            child: MyTextField(
+          controller: _messageConroller,
+          hintText: "메세지 입력",
+          obscureText: false,
+        ))
+      ],
     );
   }
 }
--- lib/service/chat/chat_services.dart
+++ lib/service/chat/chat_services.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_chapapp_firebase/models/message.dart';
 
 class ChatService {
-// 파이어스토어 인스턴스 가져오기.
+// 파이어스토어&회원 인스턴스 가져오기.
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
 //유저 스트림 가져오기
 
@@ -37,6 +40,46 @@ class ChatService {
   }
 
 //메세지 보내기
+  Future<void> sendMessage(String receiverID, message) async {
+    // 현재 유저 정보.
+    final String currentUserID = _auth.currentUser!.uid;
+    final String curreUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    //새로운 메세지
+    Message newMessage = Message(
+        senderID: currentUserID,
+        senderEmail: curreUserEmail,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp);
+
+    //채팅방 개설[2명유저] 독립성 보장
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); // ids를 분류(같은 채팅방의 2명의 유저 보장)
+    String chatRoomID = ids.join('_');
+
+    //새로운 메세지 DB에 저장.
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .add(newMessage.toMap());
+  }
 
 //메세지 받기
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID) {
+    // 두 명의 유저를 위한 채팅방 ID 만들기
+
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('-');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("messages")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
